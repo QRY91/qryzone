46e91ccd4f90182121641560d40a2e01e8e2d13c
QRY91 <hauntingqry@gmail.com>
2026-02-17 16:13:43 +0100

    add distilling style and how-to-zine

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,33 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const Prism = require("prismjs");
+const loadLanguages = require("prismjs/components/");
+
+// Load all languages we use
+loadLanguages(["bash", "javascript", "jsx", "python", "markup", "css", "go", "json"]);
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  // Auto-highlight <pre><code class="language-*"> blocks in HTML output
+  eleventyConfig.addTransform("prism-auto", function(content) {
+    if (!this.page.outputPath || !this.page.outputPath.endsWith(".html")) {
+      return content;
+    }
+    return content.replace(
+      /<pre>[\s]*<code class="language-(\w+)">([\s\S]*?)<\/code>[\s]*<\/pre>/g,
+      (match, lang, code) => {
+        const grammar = Prism.languages[lang];
+        if (!grammar) return match;
+        const decoded = code
+          .replace(/&lt;/g, "<").replace(/&gt;/g, ">")
+          .replace(/&amp;/g, "&").replace(/&quot;/g, '"')
+          .replace(/&#39;/g, "'");
+        const highlighted = Prism.highlight(decoded, grammar, lang);
+        return `<pre class="language-${lang}"><code class="language-${lang}">${highlighted}</code></pre>`;
+      }
+    );
+  });
+
   // Date filter for sitemap
   eleventyConfig.addFilter("dateISO", (date) => {
     return new Date(date).toISOString().split('T')[0];
